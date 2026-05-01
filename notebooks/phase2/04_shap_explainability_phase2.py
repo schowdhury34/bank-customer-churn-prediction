3b05c04b454bde9d21446057bd0e00b3e6e28661
schowdhury34 <schowdhury3434@gmail.com>
2026-05-02 01:06:44 +0530

    minor code annotation update

--- notebooks/phase2/04_shap_explainability_phase2.py
+++ notebooks/phase2/04_shap_explainability_phase2.py
@@ -7,6 +7,8 @@ Original file is located at
     https://colab.research.google.com/drive/17fiK182GIkqUdQoHSI2fwcGSfdYcS9Pk
 """
 
+#set project path
+
 from google.colab import drive
 import os
 drive.mount('/content/drive')
